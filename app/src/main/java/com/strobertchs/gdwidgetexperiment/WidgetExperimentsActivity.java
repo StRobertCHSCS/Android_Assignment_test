1f5cef877603e476c95c325412702c0e4cf4aa2e
CardboardTank <kscodeback@gmail.com>
2016-09-20 21:29:48 -0400

    final commit

--- app/src/main/java/com/strobertchs/gdwidgetexperiment/WidgetExperimentsActivity.java
+++ app/src/main/java/com/strobertchs/gdwidgetexperiment/WidgetExperimentsActivity.java
@@ -2,6 +2,9 @@ package com.strobertchs.gdwidgetexperiment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class WidgetExperimentsActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class WidgetExperimentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_widget_experiments);
+        Toast.makeText(getApplicationContext(), "Click \"Left Button\" to cycle text visibility", Toast.LENGTH_LONG).show();
+    }
+    public void toggleVisibility(View view)
+    {
+        TextView text = (TextView) findViewById(R.id.textView);
+        switch (text.getVisibility())
+        {
+            case View.VISIBLE:
+                text.setVisibility(View.INVISIBLE);
+                break;
+            case View.INVISIBLE:
+                text.setVisibility(View.GONE);
+                break;
+            default:
+                text.setVisibility(View.VISIBLE);
+        }
     }
 }
